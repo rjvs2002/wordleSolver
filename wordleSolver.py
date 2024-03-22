020582338a03d26811ca528976961f1eb0e086e3
rjvs2002 <rjvs2002@gmail.com>
2024-03-22 17:20:41 -0600

    Create wordleSolver.py

---
+++ wordleSolver.py
@@ -0,0 +1,42 @@
+#Imports
+import numpy as np
+
+
+#Load word bank
+def loadWordBank():
+    f = open('wordBank.txt', r)
+
+    lines = f.readlines()
+    
+    for line in lines:
+        print(line)
+
+    f.close()
+
+
+
+
+
+
+#Selects random word from word bank
+
+
+
+
+
+
+#Solver alogrithm
+
+
+
+
+#Driver function
+if __name__ == '__main__':
+    loadWordBank()
+
+
+
+
+    pass
+
+
